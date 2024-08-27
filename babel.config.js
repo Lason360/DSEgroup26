1f58ee72f4af3331dde6d8aa40e6c96bbd31272e
Siriwardhana S.M.C.R <chirantha.21@cse.mrt.ac.lk>
2024-08-28 02:04:30 +0530

    Front end added again

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
+const { default: plugin } = require("tailwindcss");
+
 module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugin: ["nativewind/babel" , 'react-native-reanimated/plugin'],
   };
 };
